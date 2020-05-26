6c469676acdf717153a3a62dc2466e25fe6d6a37
Ben Bridle <bridle.benjamin@gmail.com>
2020-05-26 07:24:57 +0000

    Remove superfluous comments

--- test.cpp
+++ test.cpp
@@ -3,21 +3,6 @@
 #include <map>
 #include "Colour.h"
 
-// default colours: "\x1b[0m"
-
-// 30: black
-// 31: red
-// 32: green
-// 33: yellow
-// 34: blue
-// 35: magenta
-// 36: cyan
-// 37: white.
-
-//std::map<std::string, std::string> foreground_colour;
-
-
-
 int main(int argc, char** argv) {
 
     Colour colour;
