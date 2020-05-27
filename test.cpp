174ae28c1d9d9abc0dfe8413e99a65b8172c1a06
Ben Bridle <bridle.benjamin@gmail.com>
2020-05-27 03:35:59 +0000

    Add header file as starting point for Fracture TUI toolkit.Contains functions to get the terminal size.

--- test.cpp
+++ test.cpp
@@ -2,10 +2,10 @@
 #include <string>
 #include <map>
 #include "TermColour.h"
+#include "Fracture.h"
 
 int main(int argc, char **argv)
 {
-
     TermColour colour;
 
     colour.setForegroundColour(colour.red);
@@ -14,5 +14,9 @@ int main(int argc, char **argv)
     std::cout << "Test2!\n";
     colour.reset();
     std::cout << "Default\n";
+
+    Fracture frac;
+    std::cout << frac.getWidth();
+
     return 0;
 }
