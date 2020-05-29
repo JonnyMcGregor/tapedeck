d24dff8d0f2255ea4e6f939d0f473623ef70bda7
JonnyMcGregor <jonny.mcgregor1@gmail.com>
2020-05-30 11:59:22 +1200

    Added constructor and member variables to Rect.

--- fracture/components/Rect.h
+++ fracture/components/Rect.h
@@ -1,2 +1,11 @@
 struct Rect {
+
+    Rect(int origin_x, int origin_y, int width, int height) {
+        x = origin_x;
+        y = origin_y;
+        this->width = width;
+        this->height = height;
+    }
+
+    int x = 0, y = 0, width = 0, height = 0;
 };
