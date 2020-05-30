7e088fd0233fc12e7e3cfe9ffc486b20726a8c01
JonnyMcGregor <jonny.mcgregor1@gmail.com>
2020-05-30 14:06:48 +1200

    Created ScreenSpaceRect
    ScreenSpaceRect is a child of Rect. Dimensions can be defined based on a distance from the screen edges. This is done by inputing negative ints to the constructor.

--- fracture/components/Rect.h
+++ fracture/components/Rect.h
@@ -1,5 +1,5 @@
+#include "Screen.h"
 struct Rect {
-
     Rect(int origin_x, int origin_y, int width, int height) {
         x = origin_x;
         y = origin_y;
@@ -8,4 +8,29 @@ struct Rect {
     }
 
     int x = 0, y = 0, width = 0, height = 0;
+};
+
+struct ScreenSpaceRect : Rect {
+    ScreenSpaceRect(int ss_left, int ss_top, int ss_width, int ss_height, Screen parent) {
+        if (ss_left >= 0)
+            x = ss_left;
+        else
+            x = parent.width - ss_left;
+        if (ss_top >= 0)
+            y = ss_top;
+        else
+            y = parent.height - ss_top;
+        if (ss_width >= 0)
+            width = ss_width;
+        else {
+            int right = ss_width + parent.width + 1;
+            width = right - ss_left;
+        }
+        if (ss_height >= 0)
+            height = ss_height;
+        else {
+            int bottom = ss_height + parent.height + 1;
+            height = bottom - ss_top;
+        }
+    }
 };
