7468a20a084de878d1486ad28524ec1dc2044245
Ben Bridle <bridle.benjamin@gmail.com>
2020-06-12 15:17:05 +1200

    Create constructors for ScreenCell
    Allows a Screencell to be created on the fly with a character and a style.

--- fracture/components/ScreenCell.h
+++ fracture/components/ScreenCell.h
@@ -4,4 +4,11 @@
 struct ScreenCell {
     char32_t character = ' ';
     TextStyle style = TextStyle{};
+
+    ScreenCell() {}
+
+    ScreenCell(char32_t character, TextStyle style) {
+        this->character = character;
+        this->style = style;
+    }
 };
