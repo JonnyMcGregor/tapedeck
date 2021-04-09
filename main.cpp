566641288ad9f393a2d76e88aabf5add766772c9
Jonny McGregor <jonny.mcgregor1@gmail.com>
2021-04-09 13:08:34 +1200

    removed main.cpp from git ignore.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "fracture/fracture.hpp"
+#include "fracture/widgets/tapedeck.hpp"
+#include <unistd.h>
+int main()
+{
+    TapeDeck tape_deck;
+    Fracture frac(tape_deck);
+
+    while(true)
+    {
+        frac.process();
+        frac.render_to_viewport();
+        usleep(1000000/60);
+    }
+}
